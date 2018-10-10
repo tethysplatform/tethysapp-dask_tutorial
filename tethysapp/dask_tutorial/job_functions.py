4550e9276aa3676920ea29f6c2b756b9c3e966d2
hoangtran <htran@aquaveo.com>
2018-10-10 11:00:09 -0600

    30 seconds! Will this work?

--- tethysapp/dask_tutorial/job_functions.py
+++ tethysapp/dask_tutorial/job_functions.py
@@ -3,30 +3,25 @@ import dask
 
 
 def inc(x):
-    time.sleep(2)
+    time.sleep(30)
     return x + 1
 
 
 def double(x):
-    time.sleep(2)
+    time.sleep(30)
     return x + 2
 
 
 def add(x, y):
-    time.sleep(4)
+    time.sleep(30)
     return x + y
-
-
-def sum(output):
-    time.sleep(60)
-    return output
 
 
 def total():
     output = []
     for x in range(3):
-        a = dask.delayed(inc)(x)
-        b = dask.delayed(double)(x)
-        c = dask.delayed(add)(a, b)
+        a = dask.delayed(inc, pure=False)(x)
+        b = dask.delayed(double, pure=False)(x)
+        c = dask.delayed(add, pure=False)(a, b)
         output.append(c)
-    return dask.delayed(sum)(output)
+    return dask.delayed(sum, pure=False)(output)
