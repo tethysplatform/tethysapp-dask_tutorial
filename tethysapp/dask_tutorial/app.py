ca0acc60537e7c477fdc1dbfe52aab6d5b29aaf1
Nathan Swain <nswain@aquaveo.com>
2022-05-22 00:34:14 -0600

    Updates for Tethys 4

--- tethysapp/dask_tutorial/app.py
+++ tethysapp/dask_tutorial/app.py
@@ -1,4 +1,6 @@
-from tethys_sdk.base import TethysAppBase, url_map_maker
+from tethys_sdk.base import TethysAppBase
+from tethys_sdk.app_settings import SchedulerSetting
+
 
 class DaskTutorial(TethysAppBase):
     """
@@ -6,48 +8,24 @@ class DaskTutorial(TethysAppBase):
     """
 
     name = 'Dask Tutorial'
-    index = 'dask_tutorial:home'
-    icon = 'dask_tutorial/images/icon.gif'
-    package = 'dask_tutorial'
+    description = ''
+    package = 'dask_tutorial'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/dask-logo.png'
     root_url = 'dask-tutorial'
-    color = '#f39c12'
-    description = 'Place a brief description of your app here.'
+    color = '#CA4D34'
     tags = ''
     enable_feedback = False
     feedback_emails = []
 
-    def url_maps(self):
-        """
-        Add controllers
-        """
-        UrlMap = url_map_maker(self.root_url)
-
-        url_maps = (
-            UrlMap(
-                name='home',
-                url='dask-tutorial',
-                controller='dask_tutorial.controllers.home'
-            ),
-            UrlMap(
-                name='run-dask',
-                url='dask-tutorial/dask/add/{job_type}',
-                controller='dask_tutorial.controllers.run_job'
-            ),
-            UrlMap(
-                name='jobs-table',
-                url='dask-tutorial/dask/jobs_table',
-                controller='dask_tutorial.controllers.jobs_table'
-            ),
-            UrlMap(
-                name='result',
-                url='dask-tutorial/dask/result/{job_id}',
-                controller='dask_tutorial.controllers.result'
-            ),
-            UrlMap(
-                name='error_message',
-                url='dask-tutorial/dask/error',
-                controller='dask_tutorial.controllers.error_message'
+    def scheduler_settings(self):
+        scheduler_settings = (
+            SchedulerSetting(
+                name='dask_primary',
+                description='Scheduler for a Dask distributed cluster.',
+                engine=SchedulerSetting.DASK,
+                required=True
             ),
         )
 
-        return url_maps
+        return scheduler_settings
--- tethysapp/dask_tutorial/controllers.py
+++ tethysapp/dask_tutorial/controllers.py
@@ -1,10 +1,9 @@
 import random
 from django.shortcuts import render, reverse, redirect
-from tethys_sdk.permissions import login_required
+from tethys_sdk.routing import controller
 from django.http.response import HttpResponseRedirect
 from django.contrib import messages
 from tethys_sdk.gizmos import Button
-from tethys_sdk.compute import get_scheduler
 from tethys_sdk.gizmos import JobsTable
 from tethys_compute.models.dask.dask_job_exception import DaskJobException
 from tethysapp.dask_tutorial.app import DaskTutorial as app
@@ -13,7 +12,7 @@ from tethysapp.dask_tutorial.app import DaskTutorial as app
 job_manager = app.get_job_manager()
 
 
-@login_required()
+@controller
 def home(request):
     """
     Controller for the app home page.
@@ -22,44 +21,44 @@ def home(request):
         display_text='Dask Delayed Job',
         name='dask_delayed_button',
         attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
             'title': 'Dask Delayed Job'
         },
-        href=reverse('dask_tutorial:run-dask', kwargs={'job_type': 'delayed'})
+        href=reverse('dask_tutorial:run_job', kwargs={'job_type': 'delayed'})
     )
 
     dask_distributed_button = Button(
         display_text='Dask Distributed Job',
         name='dask_distributed_button',
         attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
             'title': 'Dask Future Job'
         },
-        href=reverse('dask_tutorial:run-dask', kwargs={'job_type': 'distributed'})
+        href=reverse('dask_tutorial:run_job', kwargs={'job_type': 'distributed'})
     )
 
     dask_multiple_leaf_button = Button(
         display_text='Dask Multiple Leaf Jobs',
         name='dask_multiple_leaf_button',
         attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
             'title': 'Dask Multiple Leaf Jobs'
         },
-        href=reverse('dask_tutorial:run-dask', kwargs={'job_type': 'multiple-leaf'})
+        href=reverse('dask_tutorial:run_job', kwargs={'job_type': 'multiple-leaf'})
     )
 
     jobs_button = Button(
         display_text='Show All Jobs',
         name='dask_button',
         attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
             'title': 'Show All Jobs'
         },
-        href=reverse('dask_tutorial:jobs-table')
+        href=reverse('dask_tutorial:jobs_table')
     )
 
     context = {
@@ -72,13 +71,95 @@ def home(request):
     return render(request, 'dask_tutorial/home.html', context)
 
 
-@login_required()
+@controller
+def jobs_table(request):
+    # Use job manager to get all the jobs.
+    jobs = job_manager.list_jobs(order_by='-id', filters=None)
+
+    # Table View
+    jobs_table_options = JobsTable(
+        jobs=jobs,
+        column_fields=('id', 'name', 'description', 'creation_time'),
+        hover=True,
+        striped=False,
+        bordered=False,
+        condensed=False,
+        actions=['logs', 'delete'],
+        results_url='dask_tutorial:result',
+        refresh_interval=1000,
+        show_detailed_status=True,
+    )
+
+    home_button = Button(
+        display_text='Home',
+        name='home_button',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Home'
+        },
+        href=reverse('dask_tutorial:home')
+    )
+
+    context = {'jobs_table': jobs_table_options, 'home_button': home_button}
+
+    return render(request, 'dask_tutorial/jobs_table.html', context)
+
+
+@controller
+def result(request, job_id):
+    # Use job manager to get the given job.
+    job = job_manager.get_job(job_id=job_id)
+
+    # Get result and name
+    job_result = job.result
+    name = job.name
+
+    home_button = Button(
+        display_text='Home',
+        name='home_button',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Home'
+        },
+        href=reverse('dask_tutorial:home')
+    )
+
+    jobs_button = Button(
+        display_text='Show All Jobs',
+        name='dask_button',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Show All Jobs'
+        },
+        href=reverse('dask_tutorial:jobs_table')
+    )
+
+    context = {
+        'result': job_result,
+        'name': name,
+        'home_button': home_button,
+        'jobs_button': jobs_button
+    }
+
+    return render(request, 'dask_tutorial/results.html', context)
+
+
+@controller
+def error_message(request):
+    messages.add_message(request, messages.ERROR, 'Invalid Scheduler!')
+    return redirect(reverse('dask_tutorial:home'))
+
+
+@controller
 def run_job(request, job_type):
     """
     Controller for the app home page.
     """
-    # Get test_scheduler app. This scheduler needs to be in the database.
-    scheduler = get_scheduler(name='test_scheduler')
+    # Get scheduler from dask_primary setting.
+    scheduler = app.get_scheduler(name='dask_primary')
 
     if job_type.lower() == 'delayed':
         from tethysapp.dask_tutorial.job_functions import delayed_job
@@ -141,81 +222,4 @@ def run_job(request, job_type):
             )
             dask.execute(future)
 
-    return HttpResponseRedirect(reverse('dask_tutorial:jobs-table'))
-
-
-@login_required()
-def jobs_table(request):
-    # Using job manager to get all jobs in the database.
-    jobs = job_manager.list_jobs(order_by='-id', filters=None)
-    # Table View
-    jobs_table_options = JobsTable(
-        jobs=jobs,
-        column_fields=('id', 'name', 'description', 'creation_time'),
-        hover=True,
-        striped=False,
-        bordered=False,
-        condensed=False,
-        results_url='dask_tutorial:result',
-        refresh_interval=1000,
-        delete_btn=True,
-        show_detailed_status=True,
-    )
-
-    home_button = Button(
-        display_text='Home',
-        name='home_button',
-        attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
-            'title': 'Home'
-        },
-        href=reverse('dask_tutorial:home')
-    )
-
-    context = {'jobs_table': jobs_table_options, 'home_button': home_button}
-
-    return render(request, 'dask_tutorial/jobs_table.html', context)
-
-
-@login_required()
-def result(request, job_id):
-    # Using job manager to get the specified job.
-    job = job_manager.get_job(job_id=job_id)
-
-    # Get result and Key
-    job_result = job.result
-    name = job.name
-
-    home_button = Button(
-        display_text='Home',
-        name='home_button',
-        attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
-            'title': 'Home'
-        },
-        href=reverse('dask_tutorial:home')
-    )
-
-    jobs_button = Button(
-        display_text='Show All Jobs',
-        name='dask_button',
-        attributes={
-            'data-toggle': 'tooltip',
-            'data-placement': 'top',
-            'title': 'Show All Jobs'
-        },
-        href=reverse('dask_tutorial:jobs-table')
-    )
-
-    context = {'result': job_result, 'name': name, 'home_button': home_button, 'jobs_button': jobs_button}
-
-    return render(request, 'dask_tutorial/results.html', context)
-
-
-@login_required()
-def error_message(request):
-    messages.add_message(request, messages.ERROR, 'Invalid Scheduler!')
-
-    return redirect(reverse('dask_tutorial:home'))
+    return HttpResponseRedirect(reverse('dask_tutorial:jobs_table'))
