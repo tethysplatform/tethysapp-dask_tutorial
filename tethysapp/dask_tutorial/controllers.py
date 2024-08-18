a70ac59a7a1309ba85e80af4e483c0964ac49bf4
jakeymac <jakeymacaroni@gmail.com>
2024-08-17 18:49:08 -0600

    updates for tethys 4.3

--- tethysapp/dask_tutorial/controllers.py
+++ tethysapp/dask_tutorial/controllers.py
@@ -1,15 +1,14 @@
 import random
-from django.shortcuts import render, reverse, redirect
 from tethys_sdk.routing import controller
 from django.http.response import HttpResponseRedirect
 from django.contrib import messages
 from tethys_sdk.gizmos import Button
 from tethys_sdk.gizmos import JobsTable
 from tethys_compute.models.dask.dask_job_exception import DaskJobException
-from tethysapp.dask_tutorial.app import DaskTutorial as app
+from .app import App
 
 # get job manager for the app
-job_manager = app.get_job_manager()
+job_manager = App.get_job_manager()
 
 
 @controller
@@ -17,6 +16,7 @@ def home(request):
     """
     Controller for the app home page.
     """
+
     dask_delayed_button = Button(
         display_text='Dask Delayed Job',
         name='dask_delayed_button',
@@ -25,18 +25,7 @@ def home(request):
             'data-bs-placement': 'top',
             'title': 'Dask Delayed Job'
         },
-        href=reverse('dask_tutorial:run_job', kwargs={'job_type': 'delayed'})
-    )
-
-    dask_distributed_button = Button(
-        display_text='Dask Distributed Job',
-        name='dask_distributed_button',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Dask Future Job'
-        },
-        href=reverse('dask_tutorial:run_job', kwargs={'job_type': 'distributed'})
+        href=App.reverse('run_job', kwargs={'job_type': 'delayed'})
     )
 
     jobs_button = Button(
@@ -47,16 +36,15 @@ def home(request):
             'data-bs-placement': 'top',
             'title': 'Show All Jobs'
         },
-        href=reverse('dask_tutorial:jobs_table')
+        href=App.reverse('jobs_table')
     )
 
     context = {
         'dask_delayed_button': dask_delayed_button,
-        'dask_distributed_button': dask_distributed_button,
-        'jobs_button': jobs_button,
+        'jobs_button': jobs_button
     }
 
-    return render(request, 'dask_tutorial/home.html', context)
+    return App.render(request, 'home.html', context)
 
 
 @controller
@@ -72,8 +60,7 @@ def jobs_table(request):
         striped=False,
         bordered=False,
         condensed=False,
-        actions=['logs', 'delete'],
-        results_url='dask_tutorial:result',
+        results_url=f'{App.package}:result',
         refresh_interval=1000,
         show_detailed_status=True,
     )
@@ -86,12 +73,12 @@ def jobs_table(request):
             'data-bs-placement': 'top',
             'title': 'Home'
         },
-        href=reverse('dask_tutorial:home')
+        href=App.reverse('home')
     )
 
     context = {'jobs_table': jobs_table_options, 'home_button': home_button}
 
-    return render(request, 'dask_tutorial/jobs_table.html', context)
+    return App.render(request, 'jobs_table.html', context)
 
 
 @controller
@@ -111,7 +98,7 @@ def result(request, job_id):
             'data-bs-placement': 'top',
             'title': 'Home'
         },
-        href=reverse('dask_tutorial:home')
+        href=App.reverse('home')
     )
 
     jobs_button = Button(
@@ -122,7 +109,7 @@ def result(request, job_id):
             'data-bs-placement': 'top',
             'title': 'Show All Jobs'
         },
-        href=reverse('dask_tutorial:jobs_table')
+        href=App.reverse('jobs_table')
     )
 
     context = {
@@ -132,14 +119,13 @@ def result(request, job_id):
         'jobs_button': jobs_button
     }
 
-    return render(request, 'dask_tutorial/results.html', context)
+    return App.render(request, 'results.html', context)
 
 
 @controller
 def error_message(request):
     messages.add_message(request, messages.ERROR, 'Invalid Scheduler!')
-    return redirect(reverse('dask_tutorial:home'))
-
+    return App.redirect(App.reverse('home'))
 
 @controller
 def run_job(request, job_type):
@@ -147,10 +133,10 @@ def run_job(request, job_type):
     Controller for the app home page.
     """
     # Get scheduler from dask_primary setting.
-    scheduler = app.get_scheduler(name='dask_primary')
+    scheduler = App.get_scheduler(name='dask_primary')
 
     if job_type.lower() == 'delayed':
-        from tethysapp.dask_tutorial.job_functions import delayed_job
+        from .job_functions import delayed_job
 
         # Create dask delayed object
         delayed = delayed_job()
@@ -164,24 +150,4 @@ def run_job(request, job_type):
         # Execute future
         dask.execute(delayed)
 
-    elif job_type.lower() == 'distributed':
-        from tethysapp.dask_tutorial.job_functions import distributed_job, convert_to_dollar_sign
-
-        # Get the client to create future
-        try:
-            client = scheduler.client
-        except DaskJobException:
-            return redirect(reverse('dask_tutorial:error_message'))
-
-        # Create future job instance
-        future = distributed_job(client)
-        dask = job_manager.create_job(
-            job_type='DASK',
-            name='dask_distributed',
-            user=request.user,
-            scheduler=scheduler,
-        )
-        dask.process_results_function = convert_to_dollar_sign
-        dask.execute(future)
-
-    return HttpResponseRedirect(reverse('dask_tutorial:jobs_table'))
+    return HttpResponseRedirect(App.reverse('jobs_table'))
--- tethysapp/dask_tutorial/job_functions.py
+++ tethysapp/dask_tutorial/job_functions.py
@@ -1,21 +1,17 @@
 import time
 import dask
 
-
 def inc(x):
     time.sleep(3)
     return x + 1
-
 
 def double(x):
     time.sleep(3)
     return x + 2
 
-
 def add(x, y):
     time.sleep(10)
     return x + y
-
 
 def sum_up(x):
     time.sleep(5)
@@ -24,7 +20,6 @@ def sum_up(x):
 
 def convert_to_dollar_sign(result):
     return '$' + str(result)
-
 
 # Delayed Job
 def delayed_job():
@@ -35,14 +30,3 @@ def delayed_job():
         c = dask.delayed(add, pure=False)(a, b)
         output.append(c)
     return dask.delayed(sum_up, pure=False)(output)
-
-
-# Distributed Job
-def distributed_job(client):
-    output = []
-    for x in range(3):
-        a = client.submit(inc, x, pure=False)
-        b = client.submit(double, x, pure=False)
-        c = client.submit(add, a, b, pure=False)
-        output.append(c)
-    return client.submit(sum_up, output)
